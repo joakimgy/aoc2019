d4ea1ae460b092575bc3c7910751863efec7fe6a
Joakim Gyllenskepp <joakim.gyllenskepp@bekk.no>
2020-12-08 17:30:51 +0100

    Completed day 8

--- day8.py
+++ day8.py
@@ -1,27 +1,45 @@
+import copy
 filename = "day8input.txt"
 
 with open(filename) as f:
     inputarray = [line.rstrip().split(" ") for line in f]
 
 
-def task1():
+def runProgram(program):
     accumulator = 0
     index = 0
     visitedIndexes = []
     while (index not in visitedIndexes):
+        if (index >= len(program)):
+            return "Program finished running. Accumalator = " + str(accumulator)
         visitedIndexes.append(index)
-        (cmd, value) = inputarray[index]
+        (cmd, value) = program[index]
         if (cmd == "acc"):
             accumulator += int(value)
         if (cmd == "jmp"):
             index += int(value)
             continue
         index += 1
+    return "Program ran one loop. Accumulator = " + str(accumulator)
+
+
+def task1():
+    accumulator = runProgram(inputarray)
     return accumulator
 
 
 def task2():
-    return "No answer"
+    for index in range(len(inputarray)):
+        (cmd, value) = inputarray[index]
+        program = copy.deepcopy(inputarray)
+        if (cmd == "jmp"):
+            program[index][0] = "nop"
+        if (cmd == "nop"):
+            program[index][0] = "jmp"
+        output = runProgram(program)
+        if ("Program finished" in output):
+            return output
+    return "No result"
 
 
 print("Task 1 result: ", task1())
